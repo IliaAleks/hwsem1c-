dda3b88d954f8579ef6044b264e68137ee403f8f
Илья Алексеев <av98060@gmail.com>
2022-12-04 14:26:30 +0300

    Task 2

--- Program.cs
+++ Program.cs
@@ -1,13 +1,35 @@
 ﻿//Task #1
-Console.WriteLine("Введите первое число: ");
-int a=int.Parse(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int b=int.Parse(Console.ReadLine());
-if (a>b) {
-    Console.WriteLine($"max={a} min={b}");
-} else if (a<b) {
-    Console.WriteLine($"max={b} min={a}");
-} else {
-    Console.WriteLine($"max=min={a}");
-}
-    
+// Console.WriteLine("Task 1");
+// Console.WriteLine("Введите первое число: ");
+// int a=int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int b=int.Parse(Console.ReadLine());
+// if (a>b) {
+//     Console.WriteLine($"max={a} min={b}");
+// } else if (a<b) {
+//     Console.WriteLine($"max={b} min={a}");
+// } else {
+//     Console.WriteLine($"max=min={a}");
+// }
+
+
+// Task #2
+Console.WriteLine("Task 2");
+int[] mas=new int[3];
+Console.WriteLine("Введите первое число:");
+mas[0]=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+mas[1]=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+mas[2]=int.Parse(Console.ReadLine());
+int result=0;
+ for (int i=0; i<3; i++) {
+    if (result<mas[i]){
+        result=mas[i];
+    }
+ }
+ Console.WriteLine($"Максимальное число равно {result}");
+
+
+
+
