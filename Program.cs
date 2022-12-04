3932e093afc301e870c6db538e1a42ee5f6bf356
Илья Алексеев <av98060@gmail.com>
2022-12-04 14:34:35 +0300

    Task3

--- Program.cs
+++ Program.cs
@@ -13,23 +13,32 @@
 // }
 
 
-// Task #2
-Console.WriteLine("Task 2");
-int[] mas=new int[3];
-Console.WriteLine("Введите первое число:");
-mas[0]=int.Parse(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-mas[1]=int.Parse(Console.ReadLine());
-Console.WriteLine("Введите третье число");
-mas[2]=int.Parse(Console.ReadLine());
-int result=0;
- for (int i=0; i<3; i++) {
-    if (result<mas[i]){
-        result=mas[i];
-    }
- }
- Console.WriteLine($"Максимальное число равно {result}");
+// // Task #2
+// Console.WriteLine("Task 2");
+// int[] mas=new int[3];
+// Console.WriteLine("Введите первое число:");
+// mas[0]=int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// mas[1]=int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите третье число");
+// mas[2]=int.Parse(Console.ReadLine());
+// int result=0;
+//  for (int i=0; i<3; i++) {
+//     if (result<mas[i]){
+//         result=mas[i];
+//     }
+//  }
+//  Console.WriteLine($"Максимальное число равно {result}");
 
+// Task #3
+Console.WriteLine("Task 3");
+Console.WriteLine("Введите число: ");
+int a=int.Parse(Console.ReadLine());
+if (a%2==0) {
+    Console.WriteLine($"Число {a} четное");
+    } else {
+        Console.WriteLine($"Число {a} нечетное");
+    }
 
 
 
